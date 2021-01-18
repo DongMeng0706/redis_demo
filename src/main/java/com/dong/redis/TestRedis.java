7214974c2e525d3ba94fefc7e494e149a8a773d2
ElvinDong <1151608223@qq.com>
2021-01-18 16:55:07 +0800

    注释说明

--- src/main/java/com/dong/redis/TestRedis.java
+++ src/main/java/com/dong/redis/TestRedis.java
@@ -13,7 +13,9 @@ public class TestRedis {
 		ApplicationContext context=new ClassPathXmlApplicationContext("classpath:application*.xml");
 		JedisPool jedisPool = context.getBean(JedisPool.class);
 		Jedis resource = jedisPool.getResource();
+		//验证密码
 		resource.auth("redis123456");
+		//选择库
 		resource.select(1);
 		String string = resource.get("k");
 		System.out.println(string);
